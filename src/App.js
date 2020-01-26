5fbf418c841a397fe2e5326b15e890a6fa044803
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-26 15:39:35 -0600

    Code clean up

--- src/App.js
+++ src/App.js
@@ -4,31 +4,6 @@ import './App.css';
 
 import Lifecycles from './lifecycles.component';
 
-// The update phase in React is triggered by a change in state or
-// change in props.
-
-// When there is a change in the props only the part of the component that uses
-// that prop will be re rendered.
-
-// When there is a change in state then the entire component is re rendered.
-
-// The lifecycle method componentDidUpdate() runs after the component is re rendered
-// and React updates the real DOM. More of this inside of the Lifecycles.
-
-// In our App component whenever there is a change in state the entire component is
-// re rendered.
-
-// In our Lifecycles component only the props are re rendered when there is a change in
-// props for that component. More of this inside of the Lifecycles.
-
-// In our current app, if state changes inside of the App component,
-// the Lifecycles component will be re rendered as well even if there is not a change
-// in props for the Lifecycles component.
-
-// To prevent the Lifecycles component and any othere child component inside of our App component from
-// re rendering when the children do not need to be updated, we can use the shouldComponentUpdate()
-// method. More of this inside of the Lifecycles.
-
 class App extends React.Component {
   constructor() {
     super();
--- src/lifecycles.component.jsx
+++ src/lifecycles.component.jsx
@@ -20,31 +20,6 @@ class Lifecycles extends React.Component {
     console.log('componentWillUnmount!');
   }
 
-  // We can control if a component goes through the update phase or not
-  // by using the shouldComponentUpdate().
-
-  // By controling wheither or not a component goes through the update phase
-  // we only render and update that component when it needs to.
-
-  // By doing this we can increase the performance of our app.
-
-  // Remember when the parent component updates so does all of its
-  // children components. 
-
-  // The shouldComponentUpdate() method takes in two arguments.
-
-  // 1st argument is nextProps.
-
-  // 2nd argument is nextState.
-
-  // shouldComponentUpdate() returns a boolean value.
-
-  // If the boolean is true then the component will update
-  // either the state, props, or both depending on our needs and
-  // how we write our code. 
-
-  // If the boolean returns false then the component will not update.
-
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate!', nextProps);
     return nextProps.text !== this.props.text;
@@ -55,13 +30,7 @@ class Lifecycles extends React.Component {
     return (
       <div className='lifecycles'>
 
-        {/* Once the component is rendered the <h3> will
-        not have to be re rendered again unless this component 
-        has a change in state. */}
-
         <h3>LIFECYCLES COMPONENT</h3>
-
-        {/* Whenever the props change only the props will be re rendered. */}
 
         {this.props.text}
       </div>
